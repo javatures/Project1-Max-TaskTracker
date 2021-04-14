e8492b35213967bcbb6fa144e08f7b298399c399
Maxwell Lee <maxwell.lee@revature.net>
2021-04-13 20:59:09 -0700

    Added postgresql schema

---
+++ lib/bin/main/schema.sql
@@ -0,0 +1,25 @@
+create table userTypes (
+    userTypeId serial primary key,
+    typeName varchar(255) unique
+);
+
+create table users (
+    userId serial primary key,
+    userType int not null references userTypes(userTypeId),
+    userName varchar(255) unique not null,
+    userPassword varchar(255) not null,
+    fname varchar(255),
+    lname varchar(255)
+);
+
+create table tasks (
+    taskId serial primary key,
+    assigner int not null references users(userId),
+    reciever int not null references users(userId),
+    title text not null,
+    body text not null,
+    currentStatus integer not null,
+    evidence bytea
+);
+
+INSERT INTO userTypes(typeName) VALUES ('manager'), ('employee');
